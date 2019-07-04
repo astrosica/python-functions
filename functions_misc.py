9d889a766f394ba5e9ca6216dbbea4479cb1e893
Jessica Campbell <campbell@astro.utoronto.ca>
2019-07-04 14:45:19 -0400

    Add fB1950toJ2000().

--- functions_misc.py
+++ functions_misc.py
@@ -11,7 +11,8 @@ from astropy.coordinates import SkyCoord
 
 def fdegtosexa(ra_deg,dec_deg):
 	'''
-	Converts Right Ascension and Declination from decimal degrees to sexagismal format. Inputs integers, floats, lists or arrays.
+	Converts Right Ascension and Declination from decimal degrees to the esexagismal format.
+	Inputs integers, floats, lists, or arrays.
 	'''
 	
 	if (isinstance(ra_deg,float)==True) or (isinstance(ra_deg,int)==True):
@@ -45,7 +46,8 @@ def fdegtosexa(ra_deg,dec_deg):
 
 def fsexatodeg(ra_sexa,dec_sexa):
 	'''
-	Converts Right Ascension and Declination from sexagismal format to decimal degrees.
+	Converts Right Ascension and Declination from the sexagismal system to decimal degrees.
+	Inputs integers, floats, lists, or arrays.
 	'''
 	
 	if (isinstance(ra_sexa,str)==True):
@@ -209,3 +211,81 @@ def fcolour_err(mag1,mag1err,mag2,mag2err):
 	colour_err = np.sqrt(mag1err**2. + mag2err**2.)
 	
 	return colour,colour_err
+
+def fB1950toJ2000(ra_B1950,dec_B1950):
+	'''
+	Transforms Right Ascension and Declination coordinates from the B1950 system to the J2000 system.
+	
+	Input
+	ra  : list of Right Ascension coordinates in decimal degrees
+	dec : list of Declination coordinates in decimal degrees
+	
+	Output
+	ra_trans  : transformed Right Ascension coordinates in decimal degrees
+	dec_trans : transformed Declination coordinates in decimal degrees
+	'''
+	
+	def transB1950toJ2000_main(ra_J2000,dec_J2000):
+		'''
+		Converts Right Ascension and Declination coordinates from the sexagismal positions in the B1959 system to decimal degrees in the J2000 system.
+		
+		Input
+		ra_J2000: single Right Ascension position in sexagismal J2000
+		dec_J2000: single Declination position in sexagismal J2000
+		
+		Output
+		ra_deg: single Right Ascension position in decimal degree
+		dec_deg: single Declination position in decimal degrees
+		'''
+		
+		# convert decimal degrees to sexagismal format
+		ra_sexa,dec_sexa = degtosexa(ra_J2000,dec_J2000)
+		# extract RA hh:mm:ss and Dec dd:mm:ss components
+		ra_hh          = float(ra_sexa.split(" ")[0])
+		ra_mm          = float(ra_sexa.split(" ")[1])
+		ra_ss          = float(ra_sexa.split(" ")[2])
+		dec_dd         = float(dec_sexa.split(" ")[0][1:])
+		dec_mm         = float(dec_sexa.split(" ")[1])
+		dec_ss         = float(dec_sexa.split(" ")[2])
+		# create RA and Dec objects
+		ra_J2000       = tpm.HMS(hh=ra_hh,mm=ra_mm,ss=ra_ss).to_radians()
+		dec_J2000      = tpm.DMS(dd=dec_dd,mm=dec_mm,ss=dec_ss).to_radians()
+		# velocity vector
+		v5             = convert.cat2v6(ra_J2000,dec_J2000)
+		v5_fk6         = convert.convertv6(v5,s1=5,s2=6,epoch=tpm.B1950,equinox=tpm.B1950)
+		v5_fk6_ep2000  = convert.proper_motion(v5_fk6,tpm.J2000,tpm.B1950)
+		d              = convert.v62cat(v5_fk6_ep2000,C=tpm.CJ)
+		ra_new_rad     = d["alpha"]
+		ra_deg     = ra_new_rad * 180./np.pi
+		dec_new_rad    = d["delta"]
+		dec_deg        = dec_new_rad * 180./np.pi
+		
+		return ra_deg,dec_deg
+	
+	ra_J2000   = []
+	dec_J2000  = []
+	
+	if isinstance(ra_B1950,list)==True:
+		'''
+		If input is a list, iterate through each set of coordinates.
+		'''
+		for i in range(len(ra_B1950)):
+			ra_i = ra_B1950[i]
+			dec_i = dec_B1950[i]
+			ra_new_deg,dec_new_deg=transB1950toJ2000_main(ra_i,dec_i)
+			ra_J2000.append(ra_new_deg)
+			dec_J2000.append(dec_new_deg)
+		
+		ra_J2000   = np.array(ra_J2000)
+		dec_J2000  = np.array(dec_J2000)
+	
+	elif isinstance(ra_B1950,float)==True:
+		'''
+		If given a single position, transform the single set of coordinates.
+		'''
+		ra_new_deg,dec_new_deg=transB1950toJ2000_main(ra_B1950,dec_B1950)
+		
+		ra_J2000 = ra_new_deg
+		dec_J2000 = dec_new_deg
+	
+	return ra_J2000, dec_J2000
