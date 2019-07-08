9d9802421a51ffd82726c88a5c25783dd11116f1
Jessica Campbell <campbell@astro.utoronto.ca>
2019-07-08 19:22:11 -0400

    Remove photometry functions.

--- functions_misc.py
+++ functions_misc.py
@@ -224,37 +224,6 @@ def fgradient(x):
 	
 	return grad
 
-def fmagerrtosnr(magerr):
-	'''
-	Converts photometric magnitude uncertainty to signal-to-noise ratio.
-	'''
-	magsnr = 1./magerr
-	return magsnr
-
-def fmagsnrtoerr(magsnr):
-	'''
-	Converts photometric magnitude signal-to-noise ratio to uncertainty.
-	'''
-	magerr = 1./abs(magsnr)
-	return magerr
-
-def fconvert_AB_vega(mag_AB,zeropoint):
-	'''
-	Converts AB magnitudes to the Vega magnitude scale.
-	'''
-	mag_Vega = mag_AB - zeropoint
-	return mag_Vega
-
-def fcolour_err(mag1,mag1err,mag2,mag2err):
-	'''
-	Returns photometric colour (mag1-mag2) with uncertainties.
-	'''
-	
-	colour = mag1 - mag2
-	colour_err = np.sqrt(mag1err**2. + mag2err**2.)
-	
-	return colour,colour_err
-
 def fB1950toJ2000(ra_B1950,dec_B1950):
 	'''
 	Transforms Right Ascension and Declination coordinates from the B1950 system to the J2000 system.
